cb65dc8d4ddf0c01699a465652598fa755358a0a
Monica <monicasucianto123@gmail.com>
2022-06-29 19:09:50 +0800

    working on pilih kebun toolbar

---
+++ app/src/main/java/com/example/taburtuai/LoginSmartFarmingActivity.kt
@@ -0,0 +1,11 @@
+package com.example.taburtuai
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginSmartFarmingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_smart_farming)
+    }
+}
--- app/src/main/java/com/example/taburtuai/PilihKebunActivity.kt
+++ app/src/main/java/com/example/taburtuai/PilihKebunActivity.kt
@@ -3,9 +3,9 @@ package com.example.taburtuai
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class PilihKebunActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_pilih_kebun)
     }
 }
